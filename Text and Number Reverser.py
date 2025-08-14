c587f4d71ba7c1d93955136b794793352016d715
Mo <142014239+Mo200909@users.noreply.github.com>
2025-08-13 23:31:51 -0400

    Add files via upload

---
+++ Text and Number Reverser.py
@@ -0,0 +1,8 @@
+text =  input("Enter a string: ")
+
+reversed_text = text[::-1]
+print(reversed_text)
+
+number = int(input("Enter a number: "))
+reversed_number = int(str(number)[::-1])
+print(reversed_number)
